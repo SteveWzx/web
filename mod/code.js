9ec2cf93d9b6ddf614511ff6f52d1146d2a24dfe
steve <421320649@qq.com>
2015-05-06 09:03:52 +0800

    modular

--- mod/code.js
+++ mod/code.js
@@ -1,4 +1,8 @@
-(function(){
-    module.reduce();
-	module.get();
-})();
+/**
+ * Created by steve on 2015/5/5.
+ */
+//模块的加载
+(function(window, steve, undefined){
+    steve.reduce();
+    steve.get();
+})(window, module);
--- mod/mod1.js
+++ mod/mod1.js
@@ -1,3 +1,7 @@
+/**
+ * Created by steve on 2015/5/5.
+ */
+//模块的定义
 var module = (function(mod){
 
     mod.count = 0;
