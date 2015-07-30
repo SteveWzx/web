f1722b9e2b1f295a7cae28328d3796e30d8b282c
steve <421320649@qq.com>
2015-07-30 17:42:48 +0800

    steve

--- assets/src/service_temp.js
+++
@@ -1,7 +0,0 @@
-define(function(){
-    return {
-        source1:'{{each Result as value index}}'
-            +'<li><span class="zc_sp">{{value.ServiceName}}</span><em>|</em><span class="zc_sp">{{value.Short}}</span></li>'
-            +'{{/each}}'
-    };
-});
