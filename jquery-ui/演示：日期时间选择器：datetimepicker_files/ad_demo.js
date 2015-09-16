5c3284dba67d687345016544ff40d4a018644a3e
SteveWzx <421320649@qq.com>
2015-09-16 22:20:38 +0800

    det

---
+++ jquery-ui/演示：日期时间选择器：datetimepicker_files/ad_demo.js
@@ -0,0 +1,7 @@
+//760x90
+document.write("<script type=\"text/javascript\">google_ad_client = \"ca-pub-7515443544894528\";google_ad_slot = \"3438097725\";google_ad_width = 728;google_ad_height = 90;</script>");
+document.write("<script type=\"text/javascript\" src=\"//pagead2.googlesyndication.com/pagead/show_ads.js\"></script>");
+
+//document.write("<script charset=\"gbk\" src=\"http://p.tanx.com/ex?i=mm_31834074_3358239_20016592\"><\/script>");
+//浮窗
+//document.write("<script charset=\"gbk\" src=\"http://p.tanx.com/ex?i=mm_31834074_3358239_13080436\"></script>");
---
+++ jquery-ui/演示：日期时间选择器：datetimepicker_files/ca-pub-7515443544894528.js
@@ -0,0 +1,1 @@
+(google_async_config = window.google_async_config || {})['ca-pub-7515443544894528'] = {"sra_enabled":false};
